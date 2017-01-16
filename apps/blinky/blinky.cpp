e243ad9a6f72420aa476fdcf43195b986e94c304
Mathias Bjerke <mathbje@gmail.com>
2017-01-16 21:47:13 +0100

    Added Blinky example

---
+++ apps/blinky/blinky.cpp
@@ -0,0 +1,82 @@
+#include <stdint.h>
+#include <stm32f10x.h>
+#include <stm32f10x_rcc.h>
+#include <stm32f10x_gpio.h>
+#include <stddef.h>
+#include "playground.h"
+
+
+/*
+ * Code to handle unexpected errors
+ */
+struct hardfault_data_t {
+    uint32_t r0;
+    uint32_t r1;
+    uint32_t r2;
+    uint32_t r3;
+    uint32_t r12;
+    uint32_t lr;
+    uint32_t pc;
+    uint32_t psr;
+};
+
+volatile struct hardfault_data_t *hardfault_data = (volatile struct hardfault_data_t *) 0x20000800;
+
+extern "C"
+void HardFault_Handler_C(uint32_t *hardfault_args) {
+    hardfault_data->r0 = hardfault_args[0];
+    hardfault_data->r1 = hardfault_args[1];
+    hardfault_data->r2 = hardfault_args[2];
+    hardfault_data->r3 = hardfault_args[3];
+    hardfault_data->r12 = hardfault_args[4];
+    hardfault_data->lr = hardfault_args[5];
+    hardfault_data->pc = hardfault_args[6];
+    hardfault_data->psr = hardfault_args[7];
+
+    halt();
+}
+
+volatile bool run = true;
+
+
+
+
+void delay() {
+	for( int i = 0; i < 100000; i++ ) {
+		__NOP()	;
+	}
+}
+
+
+/*
+ * Entry point
+ */
+int main() {
+    SystemInit();
+
+    SCB->SHCSR |= SCB_SHCSR_USGFAULTENA_Msk | SCB_SHCSR_MEMFAULTPENDED_Msk | SCB_SHCSR_BUSFAULTENA_Msk;
+
+    RCC_APB2PeriphResetCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    RCC_APB2PeriphResetCmd(RCC_APB2Periph_GPIOA, DISABLE);
+
+    RCC_APB2PeriphResetCmd(RCC_APB2Periph_GPIOB, ENABLE);
+    RCC_APB2PeriphResetCmd(RCC_APB2Periph_GPIOB, DISABLE);
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO | RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOA, ENABLE);
+
+    GPIO_InitTypeDef init;
+    GPIO_StructInit(&init);
+    init.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_Init(GPIOC, &init);
+
+    while (run) {
+        GPIO_SetBits(GPIOC, GPIO_Pin_All);
+	    delay();
+        GPIO_ResetBits(GPIOC, GPIO_Pin_All);
+	    delay();
+    }
+
+    return 0;
+}
+
+
